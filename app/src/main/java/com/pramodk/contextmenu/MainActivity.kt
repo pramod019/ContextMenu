6593d532ac9cd3545b67a5cf694818260e54286b
pramod kumar <pramodk6001@gmail.com>
2021-02-25 16:10:26 +0530

    Initial commit

---
+++ app/src/main/java/com/pramodk/contextmenu/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.pramodk.contextmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private val contact = arrayOf<String>("Akash","Vikash","John","Rahul","Ajay")
+    private lateinit var listView:ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        listView = findViewById(R.id.listView)
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,contact)
+        listView.adapter = arrayAdapter
+        //step 1 pass the  view here
+        registerForContextMenu(listView)
+    }
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.context_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.call ->{
+                Toast.makeText(applicationContext, "call code", Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.sms ->{
+                Toast.makeText(applicationContext, "sms code", Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
